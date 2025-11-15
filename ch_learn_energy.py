1325283075c63faceef018a770b4ec1d9df6d0a1
Raghunandan Pratoori <rnp@iastate.edu>
2025-11-15 09:36:02 -0600

    adjusted lr and epochs

--- ch_learn_energy.py
+++ ch_learn_energy.py
@@ -23,8 +23,8 @@ output_dir.mkdir(parents=True, exist_ok=True)
 # Hyperparameters
 # ----------------------
 config = {
-    "learning_rate": 1e-3,
-    "epochs": 100,
+    "learning_rate": 1e-4,
+    "epochs": 5000,
     "seed": 12,
 }
 
